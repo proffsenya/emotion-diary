fba05595936d267cfa17054f8253facd33f7a7f2
Arsenii <arsenii.nsk@mail.ru>
2025-05-19 22:52:48 +0300

    add backend, navigation and fixes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
 import AnalyticsComponent from "./components/AnalyticsComponent/Analytics";
 import RecordPage from "./components/RecordPage/RecordPage";
@@ -7,22 +7,16 @@ import UserProfileComponent from "./components/UserProfileComponent/ProfilePage"
 import LoginPage from "./components/Auth/LoginPage";
 import RegisterPage from "./components/Auth/RegisterPage";
 import { AuthProvider, useAuth } from "./context/AuthContext";
+
+// Layout компонент
 function Layout({ children }) {
-
-  return (
-    <>
-
-      <main
-      >
-        {children}
-      </main>
-    </>
-  );
+  return <main>{children}</main>;
 }
 
-
+// Компонент для защищённого маршрута
 function PrivateRoute({ children }) {
   const { user } = useAuth();
+  // Если пользователь есть, показываем контент, иначе перенаправляем на страницу входа
   return user ? children : <Navigate to="/login" />;
 }
 
@@ -37,7 +31,7 @@ function App() {
 
             {/* Защищённые маршруты */}
             <Route path="/analytics" element={<PrivateRoute><AnalyticsComponent /></PrivateRoute>} />
-            <Route path="/record" element={<RecordPage />} />
+            <Route path="/record" element={<PrivateRoute><RecordPage /></PrivateRoute>} />
             <Route path="/profile" element={<PrivateRoute><UserProfileComponent /></PrivateRoute>} />
           </Routes>
         </Layout>
